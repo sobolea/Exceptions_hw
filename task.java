ea5246bcfdd47ee0a6f9a2aeb259f9bde8ed1edb
sobolea <anna.sob.200@gmail.com>
2023-09-11 01:31:23 +0300

    save to file

--- task.java
+++ task.java
@@ -10,28 +10,37 @@
 2.Приложение должно попытаться распарсить полученные значения и выделить из них требуемые параметры. Если форматы данных не совпадают, 
 нужно бросить исключение, соответствующее типу проблемы. Можно использовать встроенные типы java и создать свои. Исключение должно быть 
 корректно обработано, пользователю выведено сообщение с информацией, что именно неверно.
-Если всё введено и обработано верно, должен создаться файл с названием, равным фамилии, в него в одну строку должны записаться полученные данные, вида
-<Фамилия><Имя><Отчество><датарождения> <номертелефона><пол>
+3. Если всё введено и обработано верно, должен создаться файл с названием, равным фамилии, в него в одну строку должны записаться полученные 
+данные, вида <Фамилия><Имя><Отчество><датарождения> <номертелефона><пол>
 Однофамильцы должны записаться в один и тот же файл, в отдельные строки.
 Не забудьте закрыть соединение с файлом.
 При возникновении проблемы с чтением-записью в файл, исключение должно быть корректно обработано, пользователь должен увидеть стектрейс ошибки.*/
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.List;
 import java.util.Scanner;
 
 public class task{
     public static void main(String[] args) {
        try {
             String[] data = inputData();
-            List<Object> info = new ArrayList<>();
+            Object[] info = new Object[6];
             info = formatData(data);
-            System.out.println(info);
+           
+            PrintWriter writer = new PrintWriter(String.format("%s.txt", info[0]));
+            for (Object object : info) {
+                writer.println(object);
+            }
+            writer.close();
+            //File save = new File("C:\\Users\\annas\\OneDrive\\Рабочий стол\\gb\\исключения\\hw\\hw", String.format("%s.txt", info[0]) );
        } catch (IncorrectSizeException e) {
         System.out.println(e);
        } catch (FormatException e) {
+        System.out.println(e);
+       } catch (FileNotFoundException e) {
         System.out.println(e);
        }
         
@@ -52,28 +61,28 @@ public class task{
         }
         }
 
-    public static List<Object> formatData(String[] arr) throws FormatException{
-        List<Object> info = new ArrayList <Object>();
-        info.add(arr[0]);
-        info.add(arr[1]);
-        info.add(arr[2]);
+    public static Object[] formatData(String[] arr) throws FormatException{
+        Object[] info = new Object[6];
+        info[0]= arr[0];
+        info[1]= arr[1];
+        info[2]= arr[2];
         try {
             String[] date = arr[3].split("/");
             Calendar birth = new GregorianCalendar(Integer.parseInt(date[2]), Integer.parseInt(date[1]), Integer.parseInt(date[0]));
-            info.add(birth);
-            System.out.println(birth.get(Calendar.YEAR));
+            info[3] = birth;
+            // System.out.println(birth.get(Calendar.YEAR));
         } catch (IllegalArgumentException e) {
             throw new FormatException(String.format("Incorrect input of data. Must be dd.mm.yyyy, you have %s", arr[3]));
         }
         try {
            int nuumber = Integer.parseInt(arr[4]);
-           info.add(nuumber);
+           info[4] = nuumber;
         } catch (IllegalArgumentException e) {
             throw new FormatException("Incorrect input of phone number");
         }
-//         System.out.println(arr[5]);
-// /* не работает */        if(arr[5] == "f" || arr[5] == "m") info.add(arr[5]);
-//         else throw new FormatException(String.format("Must be f or m. You have %s", arr[5]));
+        // System.out.println(arr[5]);
+/* не работает         if(arr[5] == "f" || arr[5] == "m")*/ info[5]= arr[5];
+       // else throw new FormatException(String.format("Must be f or m. You have %s", arr[5]));
         return info;
     }
 
